7b535a1adb97fc3d60254b81e4d2dc3d500fd643
chat43KBGV <81270461+chat43KBGV@users.noreply.github.com>
2021-03-23 18:22:40 -0600

    Create init.js

---
+++ Js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyBu29RK_sGAVmfSD8iK0O_p1iLWfrxcvDk",
+    authDomain: "chat43kbgvs.firebaseapp.com",
+    projectId: "chat43kbgvs",
+    storageBucket: "chat43kbgvs.appspot.com",
+    messagingSenderId: "267894608975",
+    appId: "1:267894608975:web:5535adaf1ceb3d2420c63a",
+    measurementId: "G-J283T1E4GJ"
+    });
